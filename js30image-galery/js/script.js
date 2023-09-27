18244502601bac3a631386035f877479c4a4aa74
“Anatoli <humanities5050@gmail.com>
2023-09-27 23:31:35 +0300

    feat: implement functionality in js (Wed, Sep 27, 2023 11:31:35 PM)

--- js30image-galery/js/script.js
+++ js30image-galery/js/script.js
@@ -1,10 +1,13 @@
 'use strict'
 
 const inputField = document.querySelector('.search-input');
-const inputFieldValue = document.querySelector('.search-input').value;
+const inputFieldValue = document.getElementById('search').value;
 const clearBtn = document.querySelector('.clear-button');
-const imgSrs = document.querySelector('.test');
-const url = `https://api.unsplash.com/photos/random?query=${inputFieldValue}&client_id=kzwiJQz-L95b2MwhyG9y_bKoqVJ8aN9X_9JrtKm5JSs`;
+const searchBtn = document.getElementById('searchBtn');
+const galleryRow = document.querySelector('.gallery__row');
+const imgSrs = document.querySelector('.gallery-image');
+let url = `https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=7js_hdk6-l838D_UDUSoxpNw64QGy_fol5HOyyJ7i6I`;
+
 
 document.addEventListener('DOMContentLoaded', showFocus);
 function showFocus() {
@@ -30,10 +33,49 @@ function clearInput() {
 async function getData() {
 	const res = await fetch(url);
 	const data = await res.json();
-	showData(data);
+	let subArr = data.results;
+	showData(subArr);
 }
 getData();
 
-function showData(data) {
-	imgSrs.src = data.urls.regular;
+function showData(subArr) {
+	subArr.map(function (item) {
+		const newColumn = document.createElement('div');
+		newColumn.className = `gallery__column`;
+		const newImage = document.createElement('img')
+		newImage.className = `gallery-image`;
+		newImage.src = item.urls.regular;
+		newImage.alt = `image`;
+		newColumn.append(newImage);
+		galleryRow.append(newColumn);
+	})
 }
+
+searchBtn.addEventListener('click', getSearchData)
+async function getSearchData() {
+	let inputFieldVal = document.getElementById('search').value;
+	if (inputFieldVal !== "") {
+		let searchUrl = `https://api.unsplash.com/search/photos?query=${inputFieldVal}&per_page=30&orientation=landscape&client_id=7js_hdk6-l838D_UDUSoxpNw64QGy_fol5HOyyJ7i6I`;
+		const res = await fetch(searchUrl);
+		const data = await res.json();
+		let subArr = data.results;
+		showSearchData(subArr);
+	}
+}
+getSearchData();
+
+function showSearchData(subArr) {
+	for (let i = 0; i < subArr.length; i++) {
+		const getElem = document.getElementsByClassName('gallery-image');
+		for (let i = 0; i < getElem.length; i++) {
+			getElem[i].src = subArr[i].urls.regular;
+		}
+	}
+}
+
+document.addEventListener('keyup', function (event) {
+	if (event.code === 'Enter') {
+		getSearchData();
+	}
+})
+
